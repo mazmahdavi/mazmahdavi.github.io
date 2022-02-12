1aee0224e0b16bcbfc7115e9c717b863e80999f2
mazmahdavi <99374765+mazmahdavi@users.noreply.github.com>
2022-02-13 01:46:45 +0330

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ $k = '';
 for($i=0;$i<32;$i++)
     $k .= '11110000';
 $k = str_shuffle($k);
-echo "<textarea name=\"bin\" id=\"bin\" cols=\"70\" rows=\"10\">$k</textarea><br />\nHEX: "
-. big_base_convert($k,2,16);
+echo "<textarea name=\"bin\" id=\"bin\" cols=\"70\" rows=\"10\">$k</textarea><br />\n";
 
 ?>
